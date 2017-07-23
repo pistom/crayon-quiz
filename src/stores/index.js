d3c3662d66d852a6524801a1e01e60d260c209d9
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2017-07-23 15:39:37 +0200

    Corrected create store

--- src/stores/index.js
+++ src/stores/index.js
@@ -3,12 +3,17 @@ import promiseMiddleware from 'redux-promise-middleware';
 import reducers from '../reducers';
 
 function reduxStore(initialState) {
-  const store = createStore(reducers, initialState,
-    compose(
-      applyMiddleware(promiseMiddleware()),
-      window.devToolsExtension && window.devToolsExtension()
-    )
+
+  const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+  const enhancer = composeEnhancers(
+    applyMiddleware(promiseMiddleware()),
   );
+
+  const store = createStore(reducers, initialState, enhancer);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
